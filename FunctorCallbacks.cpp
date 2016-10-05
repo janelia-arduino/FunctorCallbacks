912e11502524b4c00e3646085db8f553d68486dd
Peter Polidoro <peterpolidoro@gmail.com>
2016-10-05 17:50:30 -0400

    Partial functionality.

--- FunctorCallbacks.cpp
+++ FunctorCallbacks.cpp
@@ -8,12 +8,69 @@
 #include "FunctorCallbacks.h"
 
 
-void FunctorCallbacks::setup()
+void FunctorCallback::attachCallback(const Functor0 & callback)
 {
-  event_array_.fill(Event());
-  removeAllEvents();
-  startTimer();
+  callback_ = callback;
 }
 
-FunctorCallbacks functor_isr;
+void FunctorCallback::callback()
+{
+  if (callback_)
+  {
+    callback_();
+  }
+}
+
+boolean FunctorCallback::callbackIsAttached()
+{
+  return callback_;
+}
+
+FunctorCallbacks::FunctorCallbacks()
+{
+  functor_callbacks_.fill(FunctorCallback());
+}
+
+void FunctorCallbacks::attachCallback(const size_t index, const Functor0 & callback)
+{
+  if (index >= FUNCTOR_CALLBACKS_COUNT)
+  {
+    return;
+  }
+  functor_callbacks_[index].attachCallback(callback);
+}
+
+void FunctorCallbacks::detachCallback(const size_t index)
+{
+  if (index >= FUNCTOR_CALLBACKS_COUNT)
+  {
+    return;
+  }
+  functor_callbacks_[index] = FunctorCallback();
+}
+
+boolean FunctorCallbacks::callbackIsAttached(const size_t index)
+{
+  if (index >= FUNCTOR_CALLBACKS_COUNT)
+  {
+    return false;
+  }
+  return functor_callbacks_[index].callbackIsAttached();
+}
+
+void FunctorCallbacks::callback(const size_t index)
+{
+  if (index >= FUNCTOR_CALLBACKS_COUNT)
+  {
+    return;
+  }
+  functor_callbacks_[index].callback();
+}
+
+FunctorCallbacks g_functor_callbacks;
+
+void gFunctorCallback0()
+{
+  g_functor_callbacks.callback(0);
+}
 
--- FunctorCallbacks.h
+++ FunctorCallbacks.h
@@ -14,18 +14,41 @@
 #endif
 #include "Array.h"
 #include "Functor.h"
-#include "Streaming.h"
 
 
-enum{FUNCTOR_CALLBACKS_COUNT_MAX=10};
+enum{FUNCTOR_CALLBACKS_COUNT=10};
 
-class FunctorCallbacks
+void gFunctorCallback0();
+void gFunctorCallback1();
+void gFunctorCallback2();
+void gFunctorCallback3();
+void gFunctorCallback4();
+void gFunctorCallback5();
+void gFunctorCallback6();
+void gFunctorCallback7();
+void gFunctorCallback8();
+void gFunctorCallback9();
+
+class FunctorCallback
 {
 public:
   void attachCallback(const Functor0 & callback);
   void callback();
+  boolean callbackIsAttached();
 private:
   Functor0 callback_;
+};
+
+class FunctorCallbacks
+{
+public:
+  FunctorCallbacks();
+  void attachCallback(const size_t index, const Functor0 & callback);
+  void detachCallback(const size_t index);
+  boolean callbackIsAttached(const size_t index);
+  void callback(const size_t index);
+private:
+  Array<FunctorCallback,FUNCTOR_CALLBACKS_COUNT> functor_callbacks_;
 };
 
 extern FunctorCallbacks g_functor_callbacks;
