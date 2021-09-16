172e531280284d2d4a98cf87cacfc7f91b4483d8
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 13:11:03 -0400

    Update email address

--- src/FunctorCallbacks.h
+++ src/FunctorCallbacks.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef FUNCTOR_CALLBACKS_H
 #define FUNCTOR_CALLBACKS_H
